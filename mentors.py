e2859042f77372a324a925fa3bd4ffa7290436dd
Fatih <121514395+buyukakcali@users.noreply.github.com>
2024-03-28 14:32:24 +0100

    - Baslangicta, kolon isimlerinin buluttaki dosyada oldugu sekilde guncellenmesi saglandi
    -Her kolondan filtrelee yapma imkani elde etmek icin methodlar yazildi/guncellendi/tasindi( to main.py)

--- mentors.py
+++ mentors.py
@@ -19,6 +19,7 @@ class MentorPage(QWidget):
 
         self.worksheet = main.connection_hub('credentials/key.json', 'Mentor', 'Sayfa1')
         self.mentees = self.worksheet.get_all_values()
+        main.write2table(self.form_mentor, [self.mentees[0]])    # This code updates the tableWidget headers
         self.menu_user = None
         self.menu_admin = None
 
@@ -28,20 +29,22 @@ class MentorPage(QWidget):
         self.form_mentor.comboBoxFilterOptions.currentIndexChanged.connect(self.filter_table)
         self.form_mentor.pushButtonBackMenu.clicked.connect(self.back_menu)
         self.form_mentor.pushButtonExit.clicked.connect(self.app_exit)
-        self.form_mentor.comboBoxFilterOptions.addItems(self.filter_options)
+        self.form_mentor.comboBoxFilterOptions.addItems(main.filter_active_options(self.mentees, self.filtering_column))
 
-    @property
-    def filter_options(self):
-        option_elements = []
-        for row in self.mentees[1:]:
-            option_elements.append(row[self.filtering_column].strip())
-        filter_options = list(set(option_elements))
-        filter_options.sort()
-        # This(two rows which are below) is an issue that is inside the code, and it is in a specific language.
-        # It must be changed while updating the application for any other language
-        filter_options.remove('Diger')
-        filter_options.append('Diger')
-        return filter_options
+        # Activity code to offer new filtering options when you click on the titles
+        self.form_mentor.tableWidget.horizontalHeader().sectionClicked.connect(self.on_header_clicked)
+
+    # This code is for cell clicking
+    # def on_item_clicked(self, item):
+    #     column_id = item.column()
+    #     QMessageBox.information(self, "Column Clicked", f"You clicked on column {column_id + 1}")
+
+    # This code is for header clicking
+    def on_header_clicked(self, logical_index):
+        self.form_mentor.comboBoxFilterOptions.clear()
+        self.filtering_column = logical_index
+        self.form_mentor.comboBoxFilterOptions.setPlaceholderText("")
+        self.form_mentor.comboBoxFilterOptions.addItems(main.filter_active_options(self.mentees, logical_index))
 
     def search(self):
         searched_mentees = [self.mentees[0]]
@@ -70,7 +73,29 @@ class MentorPage(QWidget):
         for row in self.mentees[1:]:
             if row[self.filtering_column].strip().lower() == selected_item.strip().lower():
                 filtered_data.append(row)
-        main.write2table(self.form_mentor, filtered_data)
+
+        if len(filtered_data) > 1:
+            pass
+        else:
+            no_mentee = ['No User or Mentor Found!']
+            [no_mentee.append('-') for i in range(len(self.mentees[0]) - 1)]
+            filtered_data.append(no_mentee)
+            # filtered_data.append(['No User or Mentor Found.!', '-', '-', '-', '-', '-', '-', '-', ])
+            # Above - one line - code works as same as active code. But active code is automated for cell amount
+        return main.write2table(self.form_mentor, filtered_data)
+
+    # @property     # Ex Code! It is not in use anymore. It's still at here for educational purposes
+    # def filter_options(self):
+    #     option_elements = []
+    #     for row in self.mentees[1:]:
+    #         option_elements.append(row[self.filtering_column].strip())
+    #     filter_options = list(set(option_elements))
+    #     filter_options.sort()
+    #     # This(two rows which are below) is an issue that is inside the code, and it is in a specific language.
+    #     # It must be changed while updating the application for any other language
+    #     filter_options.remove('Diger')
+    #     filter_options.append('Diger')
+    #     return filter_options
 
     def back_menu(self):
         if self.current_user[2] == "admin":
